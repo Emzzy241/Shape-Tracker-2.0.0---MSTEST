20d2429254cc33e964ec4bae1266c818e4bc6807
Emzzy241 <emzzyoluwole@gmail.com>
2023-04-19 08:34:58 +0100

    adding a testing file for each Shape

---
+++ ShapeTracker.Tests/ModelsTests/TriangleTests.cs
@@ -0,0 +1,6 @@
+// The Models Test subdirectory will hold one or more files for tests related to our Shape Tracker models. 
+// And in this file we only have one model and that will be for Triangle
+// If we later decide to expand our application and add more models, they would each have a test file within the Models subdirectory
+
+// below is the boiler plate code for our Test file
+using Microsoft.Visual
